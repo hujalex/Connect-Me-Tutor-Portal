37166e4d63c8a0fdadfae25c475011338c8fa975
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2024-10-04 20:23:07 -0500

    10/4 commit

--- components/tutor/DashboardContent.tsx
+++ components/tutor/DashboardContent.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import { Bell, ChevronDown, Plus, Link as LinkIcon, Eye, ChevronsLeft, ChevronsRight, ChevronLeft, ChevronRight } from 'lucide-react';
-import Sidebar from '@/components/ui/sidebar';
 import TutorCalendar from './TutorCalendar';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
