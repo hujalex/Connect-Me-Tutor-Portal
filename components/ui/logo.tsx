a375d851ba033e2e36975e14140f8b919068fb4d
hujalex <ahuwindsor@gmail.com>
2024-10-18 12:39:40 -0400

    added logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Search, Link as LinkIcon, Home, HelpCircle, Settings, Compass, Globe } from 'lucide-react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Image from 'next/image';
 
 export default function Logo() {
 
     return (
     <div className="p-4 flex items-center space-x-2">
-        <div className="bg-blue-500 text-white p-1 rounded">
-          <Globe size={24} />
+        <div className="text-white p-1 rounded">
+          {/* <Globe size={24} /> */}
+          <Image alt = "logo" height = "50" width = "50" src = "/logo.png"/>
         </div>
         <span className="text-xl font-semibold">Connect Me</span>
     </div>
