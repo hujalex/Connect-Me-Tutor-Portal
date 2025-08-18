c468d40a2d3b4e38e3ee00768bb40fd4d9085455
hujalex <ahuwindsor@gmail.com>
2025-08-18 19:10:05 -0400

    added enrollments to navbar for tutors

---
+++ lib/actions/enrollment.actions.ts
@@ -0,0 +1,106 @@
+// lib/admins.actions.ts
+
+// lib/student.actions.ts
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  Profile,
+  Session,
+  Notification,
+  Event,
+  Enrollment,
+  Meeting,
+} from "@/types";
+import {
+  deleteScheduledEmailBeforeSessions,
+  sendScheduledEmailsBeforeSessions,
+  updateScheduledEmailBeforeSessions,
+} from "./email.server.actions";
+import { getProfileWithProfileId, getProfileByEmail } from "./user.actions";
+import {
+  addDays,
+  format,
+  parse,
+  parseISO,
+  isBefore,
+  isAfter,
+  areIntervalsOverlapping,
+  addHours,
+  isValid,
+  setHours,
+  setMinutes,
+} from "date-fns"; // Only use date-fns
+import { toZonedTime, fromZonedTime } from "date-fns-tz";
+import ResetPassword from "@/app/(public)/set-password/page";
+import { getStudentSessions } from "./student.actions";
+import { date } from "zod";
+import { withCoalescedInvoke } from "next/dist/lib/coalesced-function";
+import toast from "react-hot-toast";
+import { DatabaseIcon } from "lucide-react";
+import { SYSTEM_ENTRYPOINTS } from "next/dist/shared/lib/constants";
+import { Table } from "../supabase/tables";
+// import { getMeeting } from "./meeting.actions";
+
+const supabase = createClientComponentClient({
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+});
+
+export async function getEnrollments(
+  tutorId: string
+): Promise<Enrollment[] | null> {
+  try {
+    // Fetch meeting details from Supabase
+    const { data, error } = await supabase
+      .from(Table.Enrollments)
+      .select(
+        `
+        id,
+        created_at,
+        summary,
+        student_id,
+        tutor_id,
+        start_date,
+        end_date,
+        availability,
+        meetingId,
+        summer_paused,
+        duration
+      `
+      )
+      .eq("tutor_id", tutorId);
+
+    // Check for errors and log them
+    if (error) {
+      console.error("Error fetching event details:", error.message);
+      return null; // Returning null here is valid since the function returns Promise<Notification[] | null>
+    }
+
+    // Check if data exists
+    if (!data) {
+      console.log("No events found:");
+      return null; // Valid return
+    }
+
+    // Mapping the fetched data to the Notification object
+    const enrollments: Enrollment[] = await Promise.all(
+      data.map(async (enrollment: any) => ({
+        createdAt: enrollment.created_at,
+        id: enrollment.id,
+        summary: enrollment.summary,
+        student: await getProfileWithProfileId(enrollment.student_id),
+        tutor: await getProfileWithProfileId(enrollment.tutor_id),
+        startDate: enrollment.start_date,
+        endDate: enrollment.end_date,
+        availability: enrollment.availability,
+        meetingId: enrollment.meetingId,
+        summerPaused: enrollment.summer_paused,
+        duration: enrollment.duration,
+      }))
+    );
+
+    return enrollments; // Return the array of enrollments
+  } catch (error) {
+    console.error("Unexpected error in getMeeting:", error);
+    return null;
+  }
+}
