3f8d27079c634ba500b9b4714b3c3a3e13b96429
hujalex <ahuwindsor@gmail.com>
2025-09-10 19:49:24 -0400

    example branch

--- components/admin/ScheduleManagement.tsx
+++ components/admin/ScheduleManagement.tsx
@@ -282,7 +282,7 @@ const Schedule = () => {
       meetings.forEach((meeting) => {
         updatedMeetingAvailability[meeting.id] = true;
       });
-
+      //
       // const requestedSessionStartTime = parseISO(session.date);\
       const requestedSessionStartTime = requestedDate;
       const requestedSessionEndTime = addHours(
