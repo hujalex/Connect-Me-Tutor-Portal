963da66695d4793f753e70f674727e88cb0e0585
hujalex <ahuwindsor@gmail.com>
2025-10-13 01:46:29 -0400

    revamped auth

--- app/api/admin/create-user/route.ts
+++ app/api/admin/create-user/route.ts
@@ -1,11 +1,32 @@
 import { getSupabase } from "@/lib/supabase-server/serverClient";
 import { NextRequest, NextResponse } from "next/server";
+import { Profile, CreatedProfileData, Availability } from "@/types";
+
+interface UserMetadata {
+  role: "Student" | "Tutor" | "Admin";
+  first_name: string;
+  last_name: string;
+  age: string;
+  grade: string;
+  gender: string;
+  start_date: string;
+  availability: Availability[];
+  parent_name: string;
+  parent_phone: string;
+  parent_email: string;
+  phone_number: string;
+  timezone: string;
+  subjects_of_interest: string[];
+  status: "Active";
+  student_number: string;
+  languages_spoken: string[];
+}
 
 export async function POST(request: NextRequest) {
   try {
-    const { email, password } = await request.json();
+    const newProfileData: CreatedProfileData = await request.json();
 
-    const userId = await createUser(email, password);
+    const userId = await createUser(newProfileData);
     return NextResponse.json(
       { success: true, userId: userId },
       { status: 200 }
@@ -21,15 +42,37 @@ export async function POST(request: NextRequest) {
 }
 
 const createUser = async (
-  email: string,
-  password: string
+  newProfileData: CreatedProfileData
 ): Promise<string | null> => {
   try {
     // Call signUp to create a new user
+    console.log("CREATING USER", newProfileData);
     const supabase = await getSupabase();
+
+    const userMetadata: UserMetadata = {
+      role: newProfileData.role,
+      first_name: newProfileData.firstName,
+      last_name: newProfileData.lastName,
+      age: newProfileData.age,
+      grade: newProfileData.grade,
+      gender: newProfileData.gender,
+      start_date: newProfileData.startDate,
+      availability: newProfileData.availability,
+      parent_name: newProfileData.parentName,
+      parent_phone: newProfileData.parentPhone,
+      parent_email: newProfileData.parentEmail,
+      phone_number: newProfileData.phoneNumber,
+      timezone: newProfileData.timezone,
+      subjects_of_interest: newProfileData.subjects_of_interest,
+      status: newProfileData.status,
+      student_number: newProfileData.studentNumber,
+      languages_spoken: newProfileData.languages_spoken,
+    };
+
     const { data, error } = await supabase.auth.admin.createUser({
-      email,
-      password,
+      email: newProfileData.email,
+      password: newProfileData.password,
+      user_metadata: userMetadata,
     });
 
     if (error) {
