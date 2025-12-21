9d69c92afefc457f14b2641c77043d4323bcac4c
hujalex <ahuwindsor@gmail.com>
2025-12-21 15:27:10 -0500

    final edits to setup

---
+++ components/emails/missing-SEF-reminder.tsx
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ * @returns SEF reminder email
+ */
+
+import { Profile } from "@/types"
+
+const SEFReminder = (tutor: Profile, student: Profile) => {
+    return (
+        <>
+        </>
+        
+    )
+}
